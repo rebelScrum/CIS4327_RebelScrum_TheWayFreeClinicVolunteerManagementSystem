686af8db716315e5b101c16c68c1a78a349348d7
Christopher Small <christopher.c.small@gmail.com>
2015-11-10 16:09:07 -0500

    Commit 11/10

--- FirstIterationVMC/FirstIterationVMC/Controllers/HomeController.cs
+++ FirstIterationVMC/FirstIterationVMC/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace FirstIterationVMC.Controllers
 {
-    [AllowAnonymous]
+   // [AllowAnonymous]
     public class HomeController : Controller
     {
         public ActionResult Index()
--- FirstIterationVMC/FirstIterationVMC/Models/IdentityModels.cs
+++ FirstIterationVMC/FirstIterationVMC/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace FirstIterationVMC.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("defaultconn", throwIfV1Schema: false)
         {
         }
 
